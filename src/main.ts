aa3ed43d010763a1ec2abdbedda517ce6487ad8e
Dominik Mądro <d.madro@ideo.pl>
2022-05-13 15:15:50 +0200

    Zmiana odstępu na 4 spacje oraz formatowanie kodu

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('Skill Manager TDD').build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+    const config = new DocumentBuilder().setTitle('Skill Manager TDD').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
