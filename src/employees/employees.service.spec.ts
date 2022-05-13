5a5362a3971278dabeab3c4d02c39aa1fc90ee8c
Dominik Mądro <d.madro@ideo.pl>
2022-05-13 17:39:49 +0200

    Wykorzystanie danych mock do testowania oraz przerobienie obecnych testów na mocki z kontekstem bazy danych

--- src/employees/employees.service.spec.ts
+++ src/employees/employees.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma.service';
 import { EmployeesService } from './employees.service';
+import { MockContext, Context, createMockContext } from '../context';
 
 describe('EmployeesService', () => {
     let service: EmployeesService;
 
+    let mockCtx: MockContext;
+    let ctx: Context;
+
     beforeEach(async () => {
+        mockCtx = createMockContext();
+        ctx = mockCtx as unknown as Context;
+
         const module: TestingModule = await Test.createTestingModule({
             providers: [EmployeesService, PrismaService],
         }).compile();
@@ -17,18 +24,25 @@ describe('EmployeesService', () => {
         expect(service).toBeDefined();
     });
 
-    it('can add new employee', async () => {
-        expect(await service.create({
-            firstName: 'John',
-            lastName: 'Doe',
-        })).toBe('Employee created');
+    it('should create new user', async () => {
+        const employee = {
+            id: 1,
+            firstName: 'Janusz',
+            lastName: 'Tracz'
+        };
+        mockCtx.prisma.employee.create.mockResolvedValue(employee);
+      
+        await expect(service.create(employee)).resolves.toEqual('Employee created');
     });
 
     it('error while add new employee', async () => {
-        expect(await service.create({
-            firstName: 'John',
-            // @ts-ignore
-            lastName: 5,
-        })).toBe('Error while creating employee');
+        const employee: any = {
+            id: 1,
+            firstName: 'Janusz',
+            lastName: 404
+        };
+        mockCtx.prisma.employee.create.mockResolvedValue(employee);
+      
+        await expect(service.create(employee)).resolves.toEqual('Error while creating employee');
     });
 });
